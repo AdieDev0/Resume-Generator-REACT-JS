bb6863fbcbdf0609778087d1550b71bea0b41f9a
AdieDev0 <devutella@gmail.com>
2024-10-13 12:40:18 +0800

    Back Later

--- resume/src/Components/Body.jsx
+++ resume/src/Components/Body.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div className='w-full h-auto'>
+        <div className='grid gap-2'>
+            <p className='text-5xl font-Ubuntu font-bold bg-gradient-to-t from-[#343434] from-40% bg-clip-text text-transparent'>Resume Generator</p>
+            <p className='text-xl font-medium text-black/70'>It only takes a few minutes to create a professional resume!</p>
+        </div>
+
+        {/* Box */}
+        <div className='flex gap-5'>
+            {/* First Table */}
+            <div>
+                <ul>
+                    
+                </ul>
+            </div>
+        </div>
+    </div>
   )
 }
 
